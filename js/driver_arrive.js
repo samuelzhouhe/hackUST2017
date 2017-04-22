d7bbee766d04cd6bf5c5e4ab8a4fe0511c6434f7
LucenZhao <zsolitarius@gmail.com>
2017-04-22 23:19:59 +0800

    driver arrive update

--- js/driver_arrive.js
+++ js/driver_arrive.js
@@ -8,26 +8,28 @@ driverApp.filter('secToTime', [function(){
 driverApp.controller('driverArriveController', function ($scope, $rootScope, $timeout, $firebaseObject, $firebaseArray) {
   $rootScope.driver_id = "idd1";
   $rootScope.order_id = "ido1";
-  $rootScope.b_signed = false;
-  $rootScope.order_ref=firebase.database().ref("order_delivery/"+$rootScope.order_id);
-  $rootScope.receiver_ref=firebase.database().ref("user")
+  $scope.b_signed = false;
+  $scope.b_other_trip = false;
+  $scope.user_ref=firebase.database().ref("driver/"+$rootScope.driver_id);
+  $scope.order_ref=firebase.database().ref("order_delivery/"+$rootScope.order_id);
+  $scope.receiver_ref=firebase.database().ref("user")
   $scope.helloMsg = "hello driver!";
   $scope.order = {
     wait_time: 0,
     overtime_price: 0,
   }
-  var curOrder = $firebaseObject($rootScope.order_ref);
+  var curUser = $firebaseObject($scope.user_ref);
+  var curOrder = $firebaseObject($scope.order_ref);
   curOrder.$loaded().then(function(){
     $scope.order["arrive_time"] = new Date(curOrder.arrive_time);
     $scope.order["depart_time"] = new Date(curOrder.depart_time);
     $scope.order["base_price"] = curOrder.base_price;
-    var curReceiver = $firebaseObject($rootScope.receiver_ref.child(curOrder.receiver));
+    var curReceiver = $firebaseObject($scope.receiver_ref.child(curOrder.receiver));
      curReceiver.$loaded().then(function(){
       $scope.receiver = {
         name: curReceiver.name,
         phone: curReceiver.phone,
       };
-      console.log($scope.receiver.name);
     });
   });
 
@@ -51,10 +53,30 @@ driverApp.controller('driverArriveController', function ($scope, $rootScope, $ti
   	var curPrice = $scope.order.overtime_price;
     if (confirm("Do you want to sign the package?") == true) {
         $timeout.cancel(mytimeout);
-        $rootScope.b_signed = false;
+        $scope.b_signed = true;
         curOrder.status = "arrived";
         curOrder.total_price = curPrice + $scope.order.base_price;
+        curOrder.deliver_time = curOrder.arrive_time + curTime * 1000;
         curOrder.$save();
+        $scope.order["deliver_time"] = new Date(curOrder.deliver_time);
+
+        // check whether there are other ongoing trips
+        
+        for (var key in curUser.package_order) {
+         	if (curUser.package_order[key] == $rootScope.order_id) {
+          	delete curUser.package_order[key];
+          }
+        }
+        curUser.$save();
+        console.log($scope.b_other_trip);
+        if ((curUser.package_order == undefined || curUser.package_order.length == 1) && curUser.passenger_order == undefined) {
+         	$scope.b_other_trip = false;
+        } else {
+        	$scope.b_other_trip = true;
+        }
+        console.log(curUser.package_order);
+        console.log(curUser.passenger_order);
+        console.log($scope.b_other_trip);
     }
   }
 
